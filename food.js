1dbe73f234bb1428b337a7c8e1e1b8a6304f4029
yujuan0215 <109050984+yujuan0215@users.noreply.github.com>
2022-12-30 16:22:38 +0800

    images

--- food.js
+++ food.js
@@ -1,5 +1,11 @@
 Food = ["拉麵", "義大利麵", "水餃", "三明治", "炒飯", "炒麵", "捲餅", "丼飯", "韓式拌飯", "關東煮", "咖哩", "飯糰", "牛肉麵", "炒泡麵", "麵線", "燴飯", "麻辣燙"]
-
+imglink = ["url('./images/ramen.jpg')", "url('./images/spaghetti.jpeg')", "url('./images/dumplings.jfif')", "url('./images/sandwich.jfif')", "url('./images/frice.jpg')"
+, "url('./images/fnoodle.jpg')", "url('./images/Burrito.jpg')", "url('./images/drice.jpg')", "url('./images/krice.jpg')"
+,"url('./images/kcook.jfif')", "url('./images/curry.jfif')", "url('./images/rice.jfif')", "url('./images/bnoodle.jfif')", "url('./images/pm.jpg')"
+, "url('./images/ms.jfif')", "url('./images/ff.jpg')", "url('./images/mlt.jpg')"]
 $(".go").click(function()
-{ $(".Ans").html(Food[Math.floor(Math.random()*17)]);
+{ 
+    var c = Math.floor(Math.random()*17)
+    $(".Ans").html(Food[c]);
+    $(".image").css("background-image", imglink[c] )
 });
